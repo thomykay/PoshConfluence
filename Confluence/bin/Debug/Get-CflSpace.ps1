9034d6346c76958336e386a547625c728ed30dc1
Thomy Kay <thomy.kay@online.de>
2011-01-30 17:39:52 +0100

    allow connection + get-CflSpace

---
+++ Confluence/bin/Debug/Get-CflSpace.ps1
@@ -0,0 +1,14 @@
+﻿function Get-CflSpace
+{
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $false, Position = 0)]
+		[string]$Name = "*",
+		
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNull()]
+		[ThomyKay.Confluence.CflSession]$Session = (Get-CflSession -Current)
+	)
+	
+	$Session.Proxy.GetSpaces($Session.Token) | Where-Object {$_.Name -like $Name}
+}
