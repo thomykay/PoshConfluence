1426294ef1531e66e65a094a6ae8e51fb178cbbf
Thomy Kay <thomy.kay@online.de>
2012-06-19 15:52:09 +0200

    Mainly (incomplete) work on Convert-CflArchiveUser.ps1

--- Confluence/ImportExport/Convert-CflArchiveUser.ps1
+++ Confluence/ImportExport/Convert-CflArchiveUser.ps1
@@ -10,11 +10,17 @@ function Convert-CflArchiveUser
 		
 		[Parameter(Mandatory = $true, Position = 1, ValueFromPipelineByPropertyName = $true)]
 		[ValidateNotNull()]
+		[Alias("Name")]
 		[string]$FromUser,
 		
 		[Parameter(Mandatory = $true, Position = 2, ValueFromPipelineByPropertyName = $true)]
+		[Alias("Value")]
 		[ValidateNotNull()]
-		[string]$ToUser
+		[string]$ToUser,
+		
+		[Parameter(Mandatory = $false)]
+		[ValidateNotNull()]
+		[string]$NewArchiveFileName
 	)
 begin
 {
@@ -23,12 +29,26 @@ begin
 	$zip = $shell.NameSpace($Path)
 	$tempDir = Join-Path ([IO.Path]::GetTempPath()) ([Guid]::NewGuid())
 	[void](New-Item $tempDir -ItemType Directory)
-	
+	$destination = $shell.NameSpace($tempDir)
+	$source = $zip.Items() | Where-Object {$_.Name -eq $fileName}
+	$destination.CopyHere($source)
+	$entityXmlSourcePath = (Join-Path $tempDir $fileName)
+	[xml]$content = Get-Content -Path $entityXmlSourcePath
+	$navigator = $content.CreateNavigator()
+	$occurences = $navigator.Select("//property[(@name='owner' or @name='user' or @name='userName' or @name='lastModifierName' or @name='creatorName') and (not(@class))]") `
+				| where {!$_.IsEmptyElement} `
 }
 process
 {
+	$thisUserOccurences = $occurences | Where {$_.Value -eq $FromUser}
+	$thisUserOccurences | %{$_.InnerXml = $ToUser}
+	#TODO: remove the matches from the collection to prevent 'uebersprechen' in the pipeline
 }
 end
 {
+	#pack into archive
+	$content.Save($entityXmlSourcePath)
+	$newContent = $shell.NameSpace($entityXmlSourcePath)
+	$source.CopyHere($newContent)
 }
 }
---
+++ Confluence/UserManagement/Unregister-CflPageWatch.ps1
@@ -0,0 +1,3 @@
+﻿#Author: thkrause
+#Date: 5/4/2012 15:11:01
+#Script: Unregister-CflPageWatch
